a5fcd465cb3b2557e138fc64b7bfd2b96446e818
Andy Dang <andykhangdang@gmail.com>
2025-03-22 18:18:54 -0400

    profile button change
    - edit profile button so that it takes to login if not logged in, student/tutor according to role

--- pages/about_us.py
+++ pages/about_us.py
@@ -16,7 +16,7 @@ load_dotenv()
 # Page setup
 st.set_page_config(page_title="About Us | TutorConnect", layout="wide")
 
-# role = st.session_state.get('role', '') or ''
+role = st.session_state.get('role', '') or ''
 
 # Page styling
 st.markdown("""
@@ -127,10 +127,16 @@ with st.container():
 
     with col_links[1]:
         if st.button("👤 Profile", use_container_width=True):
-            if role == "Student":
-                st.switch_page("pages/student_profile.py")
-            elif role == "Tutor":
-                st.switch_page("pages/tutor_profile.py")
+            if "logged_in" not in st.session_state or not st.session_state["logged_in"]:
+                st.warning("Please log in first!")
+                time.sleep(1)
+                st.switch_page("login.py")
+            else:
+                role = st.session_state.get("role")
+                if role == "Student":
+                    st.switch_page("pages/student_profile.py")
+                elif role == "Tutor":
+                    st.switch_page("pages/tutor_profile.py")
 
     with col_links[2]:
         if st.button("🎓 Tutors", use_container_width=True):
