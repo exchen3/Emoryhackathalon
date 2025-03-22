d4853db4c807634f2499b45e780f1f0c9dd62ad3
Andy Dang <andykhangdang@gmail.com>
2025-03-21 21:34:07 -0400

    3/21 9:33
    - update env host
    - update database sql user privileges
    - edit student_info_input

--- database.sql
+++ database.sql
@@ -1,33 +1,36 @@
 use emoryhackathon;
 
 drop table if exists tutor;
+drop table if exists student;
 
 CREATE TABLE tutor (
     user_id VARCHAR(100) PRIMARY KEY,
     `password` VARCHAR(255) NOT NULL,
     `name` VARCHAR(100) NOT NULL,
-    university VARCHAR(100) NOT NULL,
-    graduation_year INT NOT NULL,
-    major VARCHAR(100) NOT NULL,
-    employed_status BOOLEAN NOT NULL,
-    internships BOOLEAN NOT NULL,
-    grad_school BOOLEAN NOT NULL,
-    gpa_range VARCHAR(100) NOT NULL,
-    classes_teaching VARCHAR(255) NOT NULL,
-    bio VARCHAR(1000) NOT NULL
+    university VARCHAR(100),
+    graduation_year INT,
+    major VARCHAR(100),
+    employed_status BOOLEAN,
+    internships BOOLEAN,
+    grad_school BOOLEAN,
+    gpa_range VARCHAR(100),
+    classes_teaching VARCHAR(255),
+    bio VARCHAR(1000)
 );
 
 CREATE TABLE student (
     user_id VARCHAR(100) PRIMARY KEY,
     `password` VARCHAR(255) NOT NULL,
     `name` VARCHAR(100) NOT NULL,
-    university VARCHAR(100) NOT NULL,
-    graduation_year INT NOT NULL,
-    major VARCHAR(100) NOT NULL,
-    employed_status BOOLEAN NOT NULL,
-    internships BOOLEAN NOT NULL,
-    grad_school BOOLEAN NOT NULL,
-    gpa_range VARCHAR(100) NOT NULL,
-    classes_taking VARCHAR(255) NOT NULL,
-    bio VARCHAR(1000) NOT NULL
+    university VARCHAR(100),
+    graduation_year INT,
+    major VARCHAR(100),
+    employed_status BOOLEAN,
+    internships BOOLEAN,
+    grad_school BOOLEAN,
+    gpa_range VARCHAR(100),
+    classes_taking VARCHAR(255),
+    bio VARCHAR(1000)
 );
+
+select * from emoryhackathon.student;
