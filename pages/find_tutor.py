0a56a0e8f47d69608d6327edc342bf91509b2060
Lisa Yang <lisa.yang@emory.edu>
2025-03-22 21:23:40 -0400

    1
    1

--- pages/find_tutor.py
+++ pages/find_tutor.py
@@ -13,6 +13,10 @@ if "logged_in" not in st.session_state or not st.session_state["logged_in"]:
         st.switch_page("login.py")
     st.stop()
 
+if st.session_state["role"] == "Tutor":
+    st.warning("This page is for students only.")
+    st.stop()
+    
 username = st.session_state["username"]
 
 # Create a session factory
